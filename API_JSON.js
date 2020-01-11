8ee3ac23deabc884e2cd4e05e619692d9198d1fc
21572 <mariacatarina2001.ma@gmail.com>
2020-01-11 00:35:47 +0000

    11/01/2020
    atualização do site

---
+++ API_JSON.js
@@ -0,0 +1,10 @@
+
+
+ $.getJSON("../API.json",function(API){
+
+ document.getElementById("precos").innerHTML=API.promo[0].promo;
+
+	document.getElementById("precos1").innerHTML=API.promo[1].promo;	
+
+ console.log(API.promo[0].promo);
+});
